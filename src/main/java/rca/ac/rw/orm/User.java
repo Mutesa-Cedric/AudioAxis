716098d79e35c07f318a843fe40e39fff07e5349
Mutesa-Cedric <mutesacedric@gmail.com>
2023-01-24 00:43:42 +0200

    feat : user model & DAO

---
+++ src/main/java/rca/ac/rw/orm/User.java
@@ -0,0 +1,70 @@
+package rca.ac.rw.orm;
+
+import rca.ac.rw.orm.*;
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    private String name;
+    private String phone;
+    private String email;
+    private String profilePic;
+
+//    private ArrayList<User> followers = new ArrayList<User>();
+
+     public User(){
+
+     }
+
+    public User( String name, String phone, String email, String profilePic) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.profilePic = profilePic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+//    public ArrayList<User> getFollowers() {
+//        return followers;
+//    }
+//
+//    public void setFollowers(ArrayList<User> followers) {
+//        this.followers = followers;
+//    }
+}
