81674fd4b1e74bfd64ef97cb3f954c825efdf1eb
Bellamy <bdanbellamy@gmail.com>
2023-01-30 09:56:37 +0200

    updated the song interface,SongDaoImpl and Orm Song classes...

--- src/main/java/rca/ac/rw/dao/song/SongDaoImpl.java
+++ src/main/java/rca/ac/rw/dao/song/SongDaoImpl.java
@@ -1,4 +1,36 @@
 package rca.ac.rw.dao.song;
 
-public class SongDaoImpl {
+import javax.persistence.*;
+import java.util.List;
+
+public class SongDaoImpl implements Song {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Song> getAllSongs() {
+        return entityManager.createQuery("SELECT s FROM Song s", Song.class).getResultList();
+    }
+
+    @Override
+    public Song getSongById(int id) {
+        return entityManager.find(Song.class, id);
+    }
+
+    @Override
+    public void addSong(Song song) {
+        entityManager.persist(song);
+    }
+
+    @Override
+    public void updateSong(Song song) {
+        entityManager.merge(song);
+    }
+
+    @Override
+    public void deleteSong(int id) {
+        Song song = getSongById(id);
+        entityManager.remove(song);
+    }
 }
--- src/main/java/rca/ac/rw/orm/Song.java
+++ src/main/java/rca/ac/rw/orm/Song.java
@@ -1,12 +1,69 @@
 package rca.ac.rw.orm;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "songs")
 public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "artist")
     private String artist;
+
+    @Column(name = "path")
     private String path;
+
+    @Column(name = "url")
     private String url;
 
-    public Song(String name,String artist, String path, String url){
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Song(String name, String artist, String path, String url){
         this.name = name;
         this.artist = artist;
         this.path = path;
@@ -17,4 +74,7 @@ public class Song {
         this.artist = artist;
         this.path = path;
     }
+
+    public Song () {}
 }
+
